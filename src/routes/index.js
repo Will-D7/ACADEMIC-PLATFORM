898b2ba11d3737633d8fe9ce10ec51f22c577fb6
Stefany <espinozastefany646@gmail.com>
2023-10-19 23:29:26 -0400

    ventana de visualizacion de tareas

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,12 @@ const admin = require('firebase-admin');
 router.get('/',(req,res) =>{
     res.render('index');
 });
+router.get('/tasks',(req,res) =>{
+    res.render('tasks');
+});
+router.get('/modaltarea',(req,res) =>{
+    res.render('modaltarea');
+});
 ////////////////////////////////////////////
             ///Funciones Aquí///
 ////////////////////////////////////////////
